ee581d04ce49fa5b105cf8d62ef7425560d19102
AshishkrGoyal <15103140ashish@gmail.com>
2017-08-23 21:39:15 +0530

    online-com

--- assets/script.js
+++ assets/script.js
@@ -2,5 +2,7 @@ if(navigator.geolocation)
 {
     navigator.geolocation.getCurrentPosition(function (position) {
         console.log(position.coords.latitude, position.coords.longitude);
+        console.log("Ashish Goyal");
     })
 }
+console.log("Ashish");
