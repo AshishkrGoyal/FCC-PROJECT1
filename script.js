302d0e815c1c3ff9097531c023a0385c0e529c32
AshishkrGoyal <15103140ashish@gmail.com>
2017-08-23 19:52:32 +0530

    navigator_add

---
+++ script.js
@@ -0,0 +1,6 @@
+if(navigator.geolocation)
+{
+    navigator.geolocation.getCurrentPosition(function (position) {
+        console.log(position.coords.latitude, position.coords.longitude);
+    })
+}
