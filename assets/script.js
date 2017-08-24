07f03ac53e3689043f2bb542f2da4a05207b17cf
AshishkrGoyal <15103140ashish@gmail.com>
2017-08-24 07:13:26 +0530

    Structure_commit

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,4 @@
+/*
 if(navigator.geolocation)
 {
     navigator.geolocation.getCurrentPosition(function (position) {
@@ -5,4 +6,22 @@ if(navigator.geolocation)
         console.log("Ashish Goyal");
     })
 }
-console.log("Ashish");
+console.log("Ashish");*/
+
+
+//it is a nodeJS server for getting response in JSON format..
+var express = require('express');
+var request = require('request');
+
+var app = express();
+
+request("https://fcc-weather-api.glitch.me/api/current?lat=31.253925900000002&lon=75.7240654",function (err,res, body) {
+    if(err)
+    {
+        console.log(err);
+    }
+    else
+    {
+        console.log(JSON.parse(body));
+    }
+});
